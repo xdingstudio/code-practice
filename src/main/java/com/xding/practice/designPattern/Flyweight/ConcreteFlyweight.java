c70c3a28583fe16944d1651514e2fae7b3e39e5a
丁泽宇 <zeyuding@creditease.cn>
2017-09-26 14:17:38 +0800

    实现享元模式

---
+++ src/main/java/com/xding/practice/designPattern/Flyweight/ConcreteFlyweight.java
@@ -0,0 +1,18 @@
+package com.xding.practice.designPattern.Flyweight;
+
+/**
+ * @author xding
+ * @version 0.1 2017/9/26
+ */
+public class ConcreteFlyweight extends Flyweight {
+    public ConcreteFlyweight(String extrinsic) {
+        super(extrinsic);
+    }
+
+    @Override
+    public void operate() {
+
+    }
+
+
+}
