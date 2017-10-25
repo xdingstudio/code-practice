cca450c65aa85de9dc2970e358e512f20073d7f1
丁泽宇 <zeyuding@creditease.cn>
2017-10-25 15:19:07 +0800

    联系 AVL树 —— 带有平衡条件的二叉查找树

---
+++ src/test/java/com/xding/practice/datastructure/AvlTreeTest.java
@@ -0,0 +1,15 @@
+package com.xding.practice.datastructure;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AvlTreeTest {
+    @Test
+    public void test() throws Exception {
+        AvlTree<Integer> tree = new AvlTree<Integer>();
+        tree.insert(3);
+
+    }
+
+}
