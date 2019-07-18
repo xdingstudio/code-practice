dcd795a54f48db462d400b8fe6b5c83041a1b4b2
xding <dingzeyu@guazi.com>
2019-07-18 17:32:13 +0800

    添加 spring 状态机

--- statemachine/src/main/java/com/xding/statemachine/StatemachineApplication.java
+++ statemachine/src/main/java/com/xding/statemachine/StatemachineApplication.java
@@ -1,13 +1,26 @@
 package com.xding.statemachine;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.statemachine.StateMachine;
+
+import javax.annotation.Resource;
 
 @SpringBootApplication
-public class StatemachineApplication {
+public class StatemachineApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(StatemachineApplication.class, args);
     }
 
+    @Resource
+    private StateMachine<States, Events> stateMachine;
+
+    @Override
+    public void run(String... args) throws Exception {
+        stateMachine.start();
+        stateMachine.sendEvent(Events.PAY);
+        stateMachine.sendEvent(Events.RECEIVE);
+    }
 }
