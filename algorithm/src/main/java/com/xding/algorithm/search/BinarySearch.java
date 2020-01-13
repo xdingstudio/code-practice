7e54096d5d4e4b3b3367cd11ad1394051401d87a
xding <dingzeyu@guazi.com>
2020-01-13 17:19:30 +0800

    二分查找

---
+++ algorithm/src/main/java/com/xding/algorithm/search/BinarySearch.java
@@ -0,0 +1,32 @@
+package com.xding.algorithm.search;
+
+/**
+ * 二分查找
+ *
+ * @author xding
+ * @date 2020/1/13 17:00
+ */
+public class BinarySearch {
+
+    public int binarySearch(int[] a, int n, int val) {
+        return binarySearchInternally(a, 0, n - 1, val);
+    }
+
+    private int binarySearchInternally(int[] a, int low, int high, int val) {
+        if (low > high) {
+            return -1;
+        }
+        
+        //        int mid = (low + high) / 2;
+        // 改进加法，避免数据范围过大时加和溢出
+        int mid = low + (high - low) / 2;
+        if (a[mid] == val) {
+            return mid;
+        } else if (a[mid] < val) {
+            return binarySearchInternally(a, mid + 1, high, val);
+        } else {
+            return binarySearchInternally(a, low, mid - 1, val);
+        }
+
+    }
+}
