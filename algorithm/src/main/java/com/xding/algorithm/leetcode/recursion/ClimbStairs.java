c498a1c6ece608f6a6c6ac05f9a6c47f00d01248
xding <dingzeyu@guazi.com>
2020-05-06 01:01:14 +0800

    爬楼梯，递归

---
+++ algorithm/src/main/java/com/xding/algorithm/leetcode/recursion/ClimbStairs.java
@@ -0,0 +1,54 @@
+package com.xding.algorithm.leetcode.recursion;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 爬楼梯
+ * 假设你正在爬楼梯。需要 n 阶你才能到达楼顶。
+ * 每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
+ * 注意：给定 n 是一个正整数。
+ * <p>
+ * 示例 1：
+ * 输入： 2
+ * 输出： 2
+ * 解释： 有两种方法可以爬到楼顶。
+ * 1.  1 阶 + 1 阶
+ * 2.  2 阶
+ * <p>
+ * 示例 2：
+ * 输入： 3
+ * 输出： 3
+ * 解释： 有三种方法可以爬到楼顶。
+ * 1.  1 阶 + 1 阶 + 1 阶
+ * 2.  1 阶 + 2 阶
+ * 3.  2 阶 + 1 阶
+ * <p>
+ * 链接：https://leetcode-cn.com/problems/climbing-stairs
+ *
+ * @author xding
+ * @date 2020/5/5 16:25
+ */
+public class ClimbStairs {
+
+    Map<Integer, Integer> map = new HashMap<>();
+
+    public int climbStairs(int n) {
+        if (n <= 0) {
+            return 0;
+        }
+        if (n == 1) {
+            return 1;
+        }
+        if (n == 2) {
+            return 2;
+        }
+        if (map.get(n) != null) {
+            return map.get(n);
+        } else {
+            int result = climbStairs(n - 1) + climbStairs(n - 2);
+            map.put(n, result);
+            return result;
+        }
+    }
+}
